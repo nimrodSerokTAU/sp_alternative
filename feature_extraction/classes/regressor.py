aae535d392ba5aec575ba20dee523c27f7642c96
XseniaP <50076292+XseniaP@users.noreply.github.com>
2024-09-13 11:44:04 -0400

    separated the cases with all features and partial features

--- feature_extraction/classes/regressor.py
+++ feature_extraction/classes/regressor.py
@@ -7,48 +7,142 @@ from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 from sklearn.svm import SVR
 from typing import Literal
+from scipy.stats import pearsonr
+import tensorflow as tf
+from sklearn.preprocessing import StandardScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, LeakyReLU
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.regularizers import l2
 
 class Regressor:
-    def __init__(self, features_file: str, test_size: float) -> None:
+    '''
+    features_file: file with all features and labels
+    test_size: portion of the codes to be separated into a test set; all MSAs for that specific code would be on the same side of the train-test split
+    mode: 1 is all features, 2 is all except SoP features, 3 is only 2 SoP features'''
+    def __init__(self, features_file: str, test_size: float, mode: int = 1) -> None:
         self.features_file = features_file
         self.test_size = test_size
         # self.num_estimators = n_estimators
         self.X_train, self.X_test, self.y_train, self.y_test = None, None, None, None
         self.X, self.y, self.y_pred = None, None, None
         self.prediction = None
+        self.main_codes_train = None
+        self.file_codes_train = None
+        self.main_codes_test = None
+        self.file_codes_test = None
+
 
         df = pd.read_csv(self.features_file)
         # Check for missing values
         print("Missing values in each column:\n", df.isnull().sum())
+        corr_coefficient1, p_value1 = pearsonr(df['normalised_sop_score'], df['dpos_dist_from_true'])
+        print(f"Pearson Correlation of SOP and dpos: {corr_coefficient1:.4f}\n", f"P-value of non-correlation: {p_value1:.4f}\n")
 
         # Handle missing values (if any)
         # Example: Filling missing values with the mean (for numerical columns)
         # df['orig_tree_ll'] = df['orig_tree_ll'].fillna(df['orig_tree_ll'].mean())
 
-        # target score
-        self.y = df["score"]
-        # features
-        # self.X = df.drop(columns=['score', 'code'])
-        self.X = df.drop(columns=['score', 'code', 'taxa_num', 'pypythia_msa_difficulty','msa_path','entropy_min','entropy_pct_25'])
 
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size=self.test_size, random_state=42)
+        # all features
+        if mode == 1:
+            self.y = df["dpos_dist_from_true"]
+            self.X = df.drop(columns=['dpos_dist_from_true', 'code', 'code1', 'taxa_num', 'pypythia_msa_difficulty'])
 
-    def random_forest(self, n_estimators: int = 100) -> float:
+        # all features except 2 features of SoP
+        if mode == 2:
+            self.y = df["dpos_dist_from_true"]
+            self.X = df.drop(columns=['dpos_dist_from_true', 'code', 'code1', 'taxa_num', 'pypythia_msa_difficulty', 'sop_score', 'normalised_sop_score'])
+
+        # only 2 features of SoP
+        if mode == 3:
+            self.y = df["dpos_dist_from_true"]
+            self.X = df[['sop_score', 'normalised_sop_score']]
+
+
+        # self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size=self.test_size)
+
+        # Get unique 'code1' values
+        unique_code1 = df['code1'].unique()
+
+        # Split the unique 'code1' into training and test sets
+        train_code1, test_code1 = train_test_split(unique_code1, test_size=0.2)
+        print(f"the training set is: {train_code1} \n")
+        print(f"the testing set is: {test_code1} \n")
+
+        # Create training and test DataFrames by filtering based on 'code1'
+        train_df = df[df['code1'].isin(train_code1)]
+        test_df = df[df['code1'].isin(test_code1)]
+
+        scaler = StandardScaler()
+        # all features
+        if mode == 1:
+            self.X_train = train_df.drop(
+                columns=['dpos_dist_from_true', 'code', 'code1', 'taxa_num', 'pypythia_msa_difficulty'])
+            self.X_test = test_df.drop(
+                columns=['dpos_dist_from_true', 'code', 'code1', 'taxa_num', 'pypythia_msa_difficulty'])
+
+        # all features except 2 sop
+        if mode == 2:
+            self.X_train = train_df.drop(
+                columns=['dpos_dist_from_true', 'code', 'code1', 'taxa_num', 'pypythia_msa_difficulty','sop_score', 'normalised_sop_score'])
+            self.X_test = test_df.drop(
+                columns=['dpos_dist_from_true', 'code', 'code1', 'taxa_num', 'pypythia_msa_difficulty', 'sop_score', 'normalised_sop_score'])
+
+        # 2 sop features
+        if mode == 3:
+            self.X_train = train_df[['sop_score', 'normalised_sop_score']]
+            self.X_test = test_df[['sop_score', 'normalised_sop_score']]
+
+        self.X_train_scaled = scaler.fit_transform(self.X_train)
+        self.X_test_scaled = scaler.fit_transform(self.X_test)
+        self.main_codes_train = train_df['code1']
+        self.file_codes_train = train_df['code']
+        self.main_codes_test = test_df['code1']
+        self.file_codes_test = test_df['code']
+
+        corr_coefficient1, p_value1 = pearsonr(test_df['normalised_sop_score'], test_df['dpos_dist_from_true'])
+        print(f"Pearson Correlation of SOP and dpos in the TEST set: {corr_coefficient1:.4f}\n",
+              f"P-value of non-correlation: {p_value1:.4f}\n")
+
+        # Set train and test Labels
+        self.y_train = train_df["dpos_dist_from_true"]
+        self.y_test = test_df["dpos_dist_from_true"]
+
+        # Check the size of each set
+        print(f"Training set size: {train_df.shape}")
+        print(f"Test set size: {test_df.shape}")
+
+    def random_forest(self, n_estimators: int = 100, i: int = 0) -> float:
         # Create and fit the Random Forest Regressor
-        self.regressor = RandomForestRegressor(n_estimators=n_estimators, random_state=42)
+        self.regressor = RandomForestRegressor(n_estimators=n_estimators)
         self.regressor.fit(self.X_train, self.y_train)
 
         # Make predictions
         self.y_pred = self.regressor.predict(self.X_test)
 
+        # Create a DataFrame
+        df_res = pd.DataFrame({
+            'Code': self.main_codes_test,
+            'File': self.file_codes_test,
+            'PredictedValue': self.y_pred
+        })
+
+        # Save the DataFrame to a CSV file
+        df_res.to_csv(f'/Users/kpolonsky/Downloads/TEST/Features/prediction_{i}.csv', index=False)
+
+
         # Evaluate the model
         mse = mean_squared_error(self.y_test, self.y_pred)
         print(f"Mean Squared Error: {mse:.4f}")
+        corr_coefficient, p_value = pearsonr(self.y_test, self.y_pred)
+        print(f"Pearson Correlation: {corr_coefficient:.4f}\n", f"P-value of non-correlation: {p_value:.4f}\n")
         return mse
 
     def gradient_boost(self, n_estimators: int = 100, learning_rate: float = 0.1, max_depth: int = 3) -> float:
         # Create and fit the Gradient Boosting Regressor
-        self.regressor = GradientBoostingRegressor(n_estimators=n_estimators, learning_rate=learning_rate, max_depth=max_depth, random_state=42)
+        self.regressor = GradientBoostingRegressor(n_estimators=n_estimators, learning_rate=learning_rate, max_depth=max_depth)
         self.regressor.fit(self.X_train, self.y_train)
 
         # Make predictions
@@ -85,11 +179,98 @@ class Regressor:
         print(f"Mean Squared Error: {mse:.4f}")
         return mse
 
-    def plot_results(self, model_name: Literal["svr", "rf", "knn-r", "gbr"]) -> None:
+    def deep_learning(self, i, epochs=50, batch_size=16, validation_split=0.1, verbose=1):
+        # model = Sequential()
+
+        # model.add(Dense(64, input_dim=self.X_train_scaled.shape[1], activation='relu'))  # input layer, hidden layer with 64 units and ReLU activation
+        # model.add(
+        #     Dense(32, activation='relu'))  # hidden layer with 32 units and ReLU activation
+
+        # model.add(Dense(64, input_dim=self.X_train_scaled.shape[1], activation='relu', kernel_regularizer=l2(0.01))) #input layer, hidden layer with 64 units and ReLU activation
+        # model.add(Dense(32, activation='relu', kernel_regularizer=l2(0.01))) #hidden layer with 32 units and ReLU activation
+
+
+        # works very well but LEAKE RELU is unusually places not as an activation...
+        # model.add(Dense(64, input_dim=self.X_train_scaled.shape[1],
+        #                 activation='relu'))  # input layer, hidden layer with 64 units and ReLU activation
+        # model.add(
+        #     Dense(32, activation='relu'))  # hidden layer with 32 units and ReLU activation
+        # model.add(LeakyReLU(alpha=0.01))
+        # model.add(Dense(1)) #output layer / single unit for regression task
+
+        model = Sequential()
+        model.add(Dense(64, input_dim=self.X_train_scaled.shape[1])) #Dense Layer (fully connected layer with 64 neurons), input dimension = number of features
+        model.add(LeakyReLU(negative_slope=0.01))  # Leaky ReLU activation
+        model.add(Dropout(0.2))  # Dropout layer, 20% of the neurons in this layer are randomly set to zero during each training epoch
+        model.add(Dense(32)) # fully connected layer with 32 neurons
+        model.add(LeakyReLU(negative_slope=0.01))  # Leaky ReLU activation
+        model.add(Dropout(0.2))  # Dropout layer, 20% of the neurons in this layer are randomly set to zero during each training epoch
+        model.add(Dense(1))  # Output layer for regression
+
+        # model = Sequential()
+        # model.add(Dense(128, input_dim=self.X_train_scaled.shape[1], activation='relu',
+        #                 kernel_regularizer=l2(0.01)))  # increased layer size
+        # model.add(Dropout(0.5))  # added dropout
+        # model.add(Dense(64, activation='relu', kernel_regularizer=l2(0.01)))
+        # model.add(Dropout(0.5))  # added dropout
+        # model.add(Dense(1))  # output layer
+
+        # Optimizer
+        optimizer = Adam(learning_rate=0.001) # Use a learning rate
+        model.compile(optimizer=optimizer, loss='mean_squared_error')
+        # model.compile(optimizer='adam', loss='mean_squared_error')
+
+        # Implement early stopping
+        early_stopping = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
+
+        history = model.fit(self.X_train_scaled, self.y_train, epochs=epochs, batch_size=batch_size, validation_split=validation_split, verbose=verbose, callbacks=[early_stopping])
+
+        # Plotting training and validation loss
+        plt.plot(history.history['loss'], label='Training Loss')
+        plt.plot(history.history['val_loss'], label='Validation Loss')
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss')
+        plt.legend()
+        plt.show()
+
+        # Evaluate the model
+        loss = model.evaluate(self.X_test_scaled, self.y_test)
+        print(f"Test Loss: {loss}")
+
+        # Make predictions
+        self.y_pred = model.predict(self.X_test_scaled)
+        self.y_pred = np.ravel(self.y_pred)
+        # Create a DataFrame
+        df_res = pd.DataFrame({
+            'Code': self.main_codes_test,
+            'File': self.file_codes_test,
+            'PredictedValue': self.y_pred
+        })
+
+        # Save the DataFrame to a CSV file
+        df_res.to_csv(f'/Users/kpolonsky/Downloads/TEST/Features/prediction_DL_{i}.csv', index=False)
+
+        # Evaluate the model
+        mse = mean_squared_error(self.y_test, self.y_pred)
+        print(f"Mean Squared Error: {mse:.4f}")
+        corr_coefficient, p_value = pearsonr(self.y_test, self.y_pred)
+        print(f"Pearson Correlation: {corr_coefficient:.4f}\n", f"P-value of non-correlation: {p_value:.4f}\n")
+        return mse
+
+
+    def plot_results(self, model_name: Literal["svr", "rf", "knn-r", "gbr", "dl"]) -> None:
         # Plot results for many features
         plt.figure(figsize=(10, 6))
         plt.scatter(self.y_test, self.y_pred, color='blue', edgecolor='k', alpha=0.7)
         plt.plot([self.y_test.min(), self.y_test.max()], [self.y_test.min(), self.y_test.max()], color='red', linestyle='--')
+        corr_coefficient, _ = pearsonr(self.y_test, self.y_pred)
+        plt.text(
+            0.05, 0.95,  # Coordinates in relative figure coordinates (0 to 1)
+            f'Pearson Correlation: {corr_coefficient:.2f}',  # Text with the coefficient
+            transform=plt.gca().transAxes,  # Use axes coordinate system
+            fontsize=12,
+            verticalalignment='top'
+        )
         plt.xlabel('True Values')
         plt.ylabel('Predicted Values')
         if model_name == "svr":
@@ -100,6 +281,8 @@ class Regressor:
             title = "K-Nearest Neighbors Regression"
         elif model_name == "gbr":
             title = "Gradient Boosting Regression"
+        elif model_name == "dl":
+            title = "Deep learning"
         plt.title(f'{title}: Predicted vs. True Values')
         plt.grid(True)
         plt.show()
@@ -110,7 +293,7 @@ class Regressor:
             indices = np.argsort(importances)[::-1]
 
             # Plot feature importances
-            plt.figure(figsize=(12, 8))
+            plt.figure(figsize=(12, 11))
             plt.title(f'{title} Feature Importances')
             plt.bar(range(self.X.shape[1]), importances[indices], align='center')
             plt.xticks(range(self.X.shape[1]), self.X.columns[indices], rotation=90)
--- feature_extraction/regression_main.py
+++ feature_extraction/regression_main.py
@@ -1,14 +1,41 @@
-
-from classes import regressor
-
+import numpy as np
+from classes.regressor import Regressor
+from scipy import stats
 
 if __name__ == '__main__':
-    model = regressor.Regressor("feature_extraction/all_features_ALL_NEW_541.csv",0.1)
-    model.random_forest()
-    model.plot_results("rf")
-    model.gradient_boost()
-    model.plot_results("gbr")
-    model.support_vector()
-    model.plot_results("svr")
-    model.k_nearest_neighbors()
-    model.plot_results("knn-r")
+
+    mse_values = []
+    for i in range(5):
+        model = Regressor("/Users/kpolonsky/Downloads/TEST/combined_results_400k_features.csv", 0.2, mode=1)
+        # mse = model.random_forest(i=i)
+        mse = model.deep_learning(i=i, epochs=30)
+        mse_values.append(mse)
+        # model.plot_results("rf")
+        model.plot_results("dl")
+    print(mse_values)
+    mean_mse = np.mean(mse_values)
+    std_mse = np.std(mse_values, ddof=1)
+    benchmark_mse = 0.0001
+
+    # Perform one-sample t-test
+    t_statistic, p_value = stats.ttest_1samp(mse_values, benchmark_mse)
+
+    print(f"Mean MSE: {mean_mse}")
+    print(f"Standard Deviation of MSE: {std_mse}")
+    print(f"t-statistic: {t_statistic}")
+    print(f"p-value: {p_value}")
+
+    # Check if p-value is less than significance level (e.g., 0.05)
+    alpha = 0.05
+    if p_value < alpha:
+        print("The mean MSE is significantly different from the benchmark value.")
+    else:
+        print("The mean MSE is not significantly different from the benchmark value.")
+
+
+    # Compare standard deviation to a threshold if needed
+    threshold_std = 0.0001  # Define a threshold for acceptable variation
+    if std_mse < threshold_std:
+        print("The MSE values are consistent with low variation.")
+    else:
+        print("The MSE values show considerable variation.")
