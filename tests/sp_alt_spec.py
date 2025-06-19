7727befc8acd28ca90bce78d71f21324a923ed5d
Nimrod Serok <nimrod@getpackage.com>
2025-06-19 10:31:25 +0300

    clean

--- tests/sp_alt_spec.py
+++ tests/sp_alt_spec.py
@@ -1223,7 +1223,7 @@ def test_percentile():
     res_25: float = calc_percentile(values, 25)
     res_75: float = calc_percentile(values, 75)
     assert res_25 == 4.5
-    assert res_75 == 23
+    assert res_75 == 16.25
 
 
 ############## Doc tests:
