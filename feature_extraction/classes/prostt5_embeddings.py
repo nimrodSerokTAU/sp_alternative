702d033e53fc28ffae57921fab32a94eae8584fb
XseniaP <50076292+XseniaP@users.noreply.github.com>
2025-12-25 18:20:02 +0200

    prostt5 embeddings

---
+++ feature_extraction/classes/prostt5_embeddings.py
@@ -0,0 +1,24 @@
+from transformers import T5EncoderModel, T5Tokenizer
+import torch
+import numpy as np
+
+# Load model
+tokenizer = T5Tokenizer.from_pretrained("Rostlab/prot_t5_xl_uniref50")
+model = T5EncoderModel.from_pretrained("Rostlab/prot_t5_xl_uniref50").eval()
+
+def get_prostt5_embedding(sequence: str) -> np.ndarray:
+    sequence = " ".join(list(sequence))
+    ids = tokenizer(sequence, return_tensors="pt", add_special_tokens=True)
+    with torch.no_grad():
+        embedding = model(**ids).last_hidden_state.squeeze(0)
+    return embedding.mean(dim=0).numpy() # I can test other aggregations, such as max, min, etc.
+
+# embedding = get_prostt5_embedding("MSEQNNTEMTFQIQRIYTKDISFEAPNAPHVFQKDWMAQ")
+# print(embedding.shape)  # e.g. (1024,)
+
+def aggregate_msa_embeddings(msa_sequences):
+    seq_embeds = np.stack([get_prostt5_embedding(seq) for seq in msa_sequences])
+    return np.concatenate([
+        seq_embeds.mean(axis=0),
+        seq_embeds.std(axis=0)
+    ])
