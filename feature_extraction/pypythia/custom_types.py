f2ba7597e52b0d4f0a100582d296b65881e93be4
NS <nimrod.serok@codeflowz.io>
2025-12-29 12:47:16 +0200

    remove old feature_extraction dir.

--- feature_extraction/pypythia/custom_types.py
+++
@@ -1,8 +0,0 @@
-from typing import List, Tuple, Dict, Union
-
-FilePath = str
-FileFormat = str
-DataType = str
-Command = List[str]
-Model = Union[str, FilePath]
-Executable = str
--- main.py
+++ main.py
@@ -1,15 +1,21 @@
+import time
+
 from classes.config import Configuration
 from classes.evo_model import EvoModel
 from enums import SopCalcTypes, WeightMethods, StatsOutput
 from multi_msa_service import multiple_msa_calc_features_and_labels
 
 configuration: Configuration = Configuration([EvoModel(-10, -0.5, 'BLOSUM62'), EvoModel(-10, -0.5, 'BLOSUM50')],
-                                             SopCalcTypes.EFFICIENT, 'comparison_files',
+                                             SopCalcTypes.EFFICIENT, '../../20251113/10686/one',
                                              {WeightMethods.HENIKOFF_WG, WeightMethods.HENIKOFF_WOG, WeightMethods.CLUSTAL_MID_ROOT,
                                               WeightMethods.CLUSTAL_DIFFERENTIAL_SUM},
                                              [5, 10, 20], {StatsOutput.ALL})
 
 if __name__ == '__main__':
+    start_time = time.time()
     multiple_msa_calc_features_and_labels(configuration)
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"Elapsed time: {elapsed_time:.4f} seconds")
 
 # TODO: consider alternatives creation.
