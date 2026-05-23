42a03a453d5d4b3b958263810d8c1ba95c29e523
XseniaP <50076292+XseniaP@users.noreply.github.com>
2026-05-23 23:39:58 +0900

    sum_of_pairs_score option for nucleotides added

--- dl_model/scripts/pickbest_main.py
+++ dl_model/scripts/pickbest_main.py
@@ -2,14 +2,12 @@ from dl_model.evaluation.pick_best import PickBest
 from dl_model.config.config import PickBestConfig
 
 pickbest_cfg = PickBestConfig(
-    features_file="/Users/kpolonsky/Documents/sp_alternative/dl_model/out/clean_data/nucleotide_features_200426.csv", #replace with your features file
-    # features_file="/Users/kpolonsky/Documents/sp_alternative/dl_model/out/large_trees_5000S1_0.5_features.csv",
+    features_file="../balibase_features_w_aligner_090426.csv", #replace with your features file
     true_score_name="dseq_from_true",
-    prediction_file=f"/Users/kpolonsky/Documents/sp_alternative/dl_model/out/NUCLEOTIDES/model1_PAM200_SoP/prediction_DL_0_mode1_dseq_from_true.csv", #replace with your prediction file
-    # prediction_file=f"/Users/kpolonsky/Documents/sp_alternative/dl_model/out/new_model2_3_340/pretrained_5000/prediction_pretrained_5000_0.5_mode1_dseq_from_true.csv", #replace with your prediction file
+    prediction_file=f"../prediction_DL_0_mode1_dseq_from_true.csv", #replace with your prediction file
     error=0.0,
     subset=None,
-    out_dir="/Users/kpolonsky/Documents/sp_alternative/dl_model/out/",
+    out_dir="../out/", #replace with your output folder path
     num_trials=1
 )
 
@@ -20,8 +18,7 @@ if __name__ == '__main__':
         pickme = PickBest(features_file=pickbest_cfg.features_file,
                             prediction_file=pickbest_cfg.prediction_file,
                             true_score_name=pickbest_cfg.true_score_name,
-                            # sum_of_pairs_score='sp_Nucleotides_GO_-5_GE_-1', #TODO - remove for AA
-                            sum_of_pairs_score="sp_NucleotidesPAM250_GO_-1.5_GE_0",
+                            # sum_of_pairs_score="sp_NucleotidesPAM250_GO_-1.5_GE_0", #TODO - add for nucleotides
                             error=pickbest_cfg.error,
                             subset=pickbest_cfg.subset,
                             output_dir=pickbest_cfg.out_dir)
